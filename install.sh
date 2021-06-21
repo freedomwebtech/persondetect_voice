09059c78881da94fb82e0597272149592b38d0b3
freedomwebtech <48402744+freedomwebtech@users.noreply.github.com>
2021-06-21 08:15:39 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,12 @@
+sudo pip3 install face_recognition
+
+sudo pip3 install gtts
+
+sudo apt install mpg321
+
+sudo apt-get install libatlas-base-dev
+
+sudo mkdir /home/pi/persondetect_voice/unknown_people
+
+
+sudo mkdir /home/pi/persondetect_voice/known_people
